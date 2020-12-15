b85fcff75b7300267d4c59eedaf028dec694f819
Gregory Nguenang <njethur@gmail.com>
2020-12-14 20:04:03 -0600

    Uploading files needed for the project

---
+++ Develop/script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var lowercaseChar = "abcdefghijklmnopqstuvwxyz"; 
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "0123456789";
+var specialChar = "~`!@#$%^&*()_-+={}[]:;'<>,.?/|";
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
